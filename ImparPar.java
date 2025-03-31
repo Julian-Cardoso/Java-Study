a07df05a3ef358e0cfc5972926b8422b508d5063
Julian Cardoso <juliancesarcardoso@gmail.com>
2025-03-31 09:53:37 -0300

    :sparkles:feat: Impar ou Par concluído

---
+++ ImparPar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ImparPar {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+
+    while(true) {
+      int n1 = scanner.nextInt();
+        if(n1 % 2 == 1){
+          System.out.println("impar");
+        } else {
+          System.out.println("par");
+        }
+    }
+  }
+}
